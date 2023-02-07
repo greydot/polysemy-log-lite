0895a935c39fecf9817bcf865350ff9ef43b3a44
Lana Black <lanablack@amok.cc>
2023-02-07 10:08:35 +0000

    Export Types.

--- src/Polysemy/Log.hs
+++ src/Polysemy/Log.hs
@@ -3,3 +3,4 @@ module Polysemy.Log (module Export) where
 import Polysemy.Log.Effect as Export (Logger)
 import Polysemy.Log.Interpreter as Export
 import Polysemy.Log.Utils as Export
+import Polysemy.Log.Types as Export
